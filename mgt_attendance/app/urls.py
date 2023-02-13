f8c159d7b869bff13a78fcadfe8b1c938299e0f3
VoDinhNghia <vodinhnghia85@gmail.com>
2023-02-13 14:17:45 +0700

    Refactor code and add tags to swagger

--- mgt_attendance/app/urls.py
+++ mgt_attendance/app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Attendance.index, name='index'),
-    path('login/', views.Attendance.as_view(), name='login'),           
+    path('', views.Attendance.as_view(), name='login'),           
 ]
